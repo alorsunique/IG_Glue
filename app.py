94d3f1f57872977ec41c1041426d7885cfe6968f
alorsunique <sounique7800@gmail.com>
2024-07-18 21:27:44 +0800

    2024 July 18

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -18,9 +18,39 @@ class Todo(db.Model):
 
 
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
+
+    if request.method == 'POST':
+        task_content = request.form['content']
+
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
     return render_template("index.html")
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+
+        return redirect('/')
+
+    except:
+        return "Error"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
